dd76ebfa90507e54ebafaaeeb589db3562454dac
Harman <reachhharman@gmail.com>
2018-02-17 20:03:55 +0530

    Completed leap year question

--- leap/leap.py
+++ leap/leap.py
@@ -1,2 +1,15 @@
 def is_leap_year(year):
-    pass
+    try:
+        if (year % 100 == 0):
+            if (year % 400 ==0):
+                return True
+            else:
+                return False
+        else:
+            if (year % 4 == 0):
+                return True
+            else:
+                return False
+
+    except ValueError as err:
+        raise Exception("Incorrect number inserted, please try again\n Error:", err)
