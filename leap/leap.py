c60d92c4533dc00d44fb87c6dedaf4d5951cab79
Harman <reachhharman@gmail.com>
2018-02-17 20:25:42 +0530

    Better code for leap year

--- leap/leap.py
+++ leap/leap.py
@@ -1,15 +1,6 @@
 def is_leap_year(year):
     try:
-        if (year % 100 == 0):
-            if (year % 400 ==0):
-                return True
-            else:
-                return False
-        else:
-            if (year % 4 == 0):
-                return True
-            else:
-                return False
-
+        return (year % 4 == 0 and (year % 100 != 0 or year % 400 == 0))
+        
     except ValueError as err:
         raise Exception("Incorrect number inserted, please try again\n Error:", err)
