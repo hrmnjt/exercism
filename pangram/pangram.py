713d7be898be905d444b300b97b29d04ca0f368a
Harman <reachhharman@gmail.com>
2018-02-17 22:37:46 +0530

    Completed problem: pangram

--- pangram/pangram.py
+++ pangram/pangram.py
@@ -1,2 +1,16 @@
 def is_pangram(sentence):
-    pass
+    try:
+        lowercase_sentence = sentence.lower()
+        
+        alphabets = "abcdefghijklmnoprstuvwxyz"
+        
+        for char in alphabets:
+            if char not in lowercase_sentence:
+                return False
+            else:
+                continue
+        
+        return True
+    
+    except SyntaxError as err:
+        raise Exception("Syntax error.\n Error: ", err)
