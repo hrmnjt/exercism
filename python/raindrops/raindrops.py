b8e8d0cf439af47c165a2097038bce491d79af00
hrmnjt <harman@hrmnjt.dev>
2024-03-09 00:19:57 +0400

    incr: more raindrop python variants

--- python/raindrops/raindrops.py
+++ python/raindrops/raindrops.py
@@ -1,4 +1,7 @@
 def convert(number):
+    """
+    an inferior approach - very verbose
+
     mod_3 = number % 3
     mod_5 = number % 5
     mod_7 = number % 7
@@ -13,3 +16,29 @@ def convert(number):
         if mod_7 == 0:
             result += "Plong"
         return(result)
+    """
+
+    result = ""
+
+    if number % 3 == 0: result += "Pling"
+    if number % 5 == 0: result += "Plang"
+    if number % 7 == 0: result += "Plong"
+
+    return result if result else str(number)
+
+    """
+    "dig deeper" section suggests below. Benefit, need to play with dict to 
+    change/add more conditions.
+
+    def convert(number):
+
+    sounds = {3: 'Pling',
+              5: 'Plang', 
+              7: 'Plong'}
+
+    results = ''.join(sounds[divisor] for 
+                      divisor in sounds.keys()
+                      if number % divisor == 0)
+
+    return results or str(number)
+    """
